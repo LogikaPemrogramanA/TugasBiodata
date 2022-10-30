e0effea187d5ceac69ca1eb7de8cbba7e7362dd7
dhamarsw <2010312101@mahasiswa.upnvj.ac.id>
2022-10-30 11:52:31 +0700

    tugas biodata dhamarsw

---
+++ tugas biodata dhamar.py
@@ -0,0 +1,10 @@
+----------------------------------------
+|No | Data  | Isian|
+|---|:-------:|------:|
+|1. |Nama     | Dhamar Satrio Widodo |
+|2.| NIM        | 2010312101 |
+|3. |Link github | https://github.com/dhamarsw |
+|4.| Link Edabit | https://edabit.com/user/X9N5XuroS8zaYfkdd |
+|5|Exp Edabit   | 75 |
+|6| Link Codewars| https://www.codewars.com/users/dhamarsw |
+|7| Level Jyu Codewars| 8kyu |
